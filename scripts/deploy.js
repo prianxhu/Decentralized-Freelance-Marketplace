3349bcd748e769ad8d54c83abc4a40a272af8489
Priyanshu Singh <0105CD231155@oriental.ac.in>
2025-06-26 14:51:23 +0530

    Add files via upload

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+const hre = require("hardhat");
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log("Deploying contract with account:", deployer.address);
+
+  const ContractFactory = await hre.ethers.getContractFactory("DecentralizedFreelanceMarketplace");
+  const contract = await ContractFactory.deploy();
+
+  await contract.deployed();
+
+  console.log("Contract deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
